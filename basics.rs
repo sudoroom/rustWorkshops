14762af0aacd43fb041122b834aa6aa973a3592f
romy <romy@snowyla.com>
2024-06-26 12:12:57 -0700

    created a new cargo sudoroom1

--- basics.rs
+++ basics.rs
@@ -21,8 +21,19 @@ fn main() {
         // note that rust is strongly typed unlike javascript or python
         let mut sum = 0.0;
         for i in 0..5 {
+            // rust is explicit - it will not silently conver the integer into a float for you
+            // cast as a floating point value
             sum += i as f64;
         }
         println!("sum is {}", sum);
+
     }
+
+    let result = sqr(2);
+    println!("Square is {}", res);
+}
+
+// explicit function types
+fun sqr(x: f64) -> f64 {
+    return x * x
 }
