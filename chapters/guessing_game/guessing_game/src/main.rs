64fbdab2bc6c82adff035308f479ea3b0c0c9007
romy <romy@snowyla.com>
2024-07-14 20:31:50 -0700

    super simple guessing game

--- chapters/guessing_game/guessing_game/src/main.rs
+++ chapters/guessing_game/guessing_game/src/main.rs
@@ -2,12 +2,25 @@ use std::io;
 
 fn main() {
     println!("guess the  number!");
-
+    let target = 42;
     println!("please input your guess.");
 
     let mut guess = String::new();
 
-    io::stdin().read_line(&mut guess).expect("Failed to read line");
+    io::stdin().read_line(&mut guess)
+                .expect("Failed to read line");
 
     println!("you guessed: {}", guess);
+
+    match guess.trim().parse::<i32>() {
+        Ok(number) => {
+            if number == target {
+                println!("The input matches {}", number);
+            } else {
+                println!("You didn't get it");
+            }
+        },
+        Err(_) => println!("That's not the valid number"),
+    }
+
 }
