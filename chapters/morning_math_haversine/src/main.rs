5825179cb58364c256069443e2d4e68d320614e3
romy <romy@snowyla.com>
2024-07-17 12:10:59 -0700

    distance earth paris

--- chapters/morning_math_haversine/src/main.rs
+++ chapters/morning_math_haversine/src/main.rs
@@ -1,4 +1,6 @@
 
+const EARTH_RADIUS_KILOMETER: f64 = 6371.0_f64;
+
 struct Coordinate {
     latitude: f64, 
     longitude: f64,
@@ -15,7 +17,7 @@ impl Coordinate {
 }
 
 // destructuring - fn calculate_distance((latitude1, longitude1): (f64, f64), (latitude2, longitude2): (f64, f64)) -> f64 {
-fn calculateDistance(location1: (f64, f64), location2: (f64, f64)) -> f64 {
+fn calculate_distance(location1: (f64, f64), location2: (f64, f64)) -> f64 {
     // 
     let (latitude1_degrees, longitude1_degrees) = location1;
     let (latitude2_degrees, longitude2_degrees) = location2;
@@ -27,16 +29,23 @@ fn calculateDistance(location1: (f64, f64), location2: (f64, f64)) -> f64 {
     let delta_longitude = (longitude1_degrees - longitude2_degrees).to_radians();
 
     let central_angle_inner = (delta_latitude / 2.0).sin().powi(2) + 
-        latitude1.cos() * latitude2.cos() & (delta_longitude/2.0).sin().powi(2);
+        latitude1.cos() * latitude2.cos() * (delta_longitude/2.0).sin().powi(2);
 
     let central_angle = 2.0 * central_angle_inner.sqrt().asin();
 
     let distance = EARTH_RADIUS_KILOMETER * central_angle;
+    distance
 }
 
-const EARTH_RADIUS_KILOMETER = 6371.0_f64;
 
 fn main() {
+   online_sample();
+
+   let distance_paris_sudoroom = calculate_distance((835004_f64, -122.2641282_f64), (48.85341_f64, -2.34880_f64));
+   println!("The distance from paris to sudoroom is {} kilometers", distance_paris_sudoroom);
+}
+
+fn online_sample() {
     let earth_radius_kilometer = 6371.0_f64;
 
     let (paris_latitude_degrees, paris_longitude_degrees) = (48.85341_f64, -2.34880_f64);
